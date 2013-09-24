926e10d7abe3191cacd718346d7f5dedf3289157
Christian Brückner <family.brueckner@googlemail.com>
2013-09-24 11:45:23 +0200

    removed some unused code

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 set :application, "your-application-name"
-set :repository, "git@github.com:you/your-project.git"
+
+# git
 set :scm, :git
+set :repository, "git@github.com:you/your-project.git"
 set :branch, 'your-branch'
 set :deploy_via, :remote_cache
 
--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,5 +1,3 @@
 set :deploy_to, '/path/to/deployments/'
 set :user, 'the_ssh_or_ftp_user'
 server 'the_host.tld', :app
-# if required
-#set :scm_passphrase, "the_secret_password"
